47016348a7dcf8515ac1dce940e056fa4ea3f07b
naga4103 <pulugunaga4103@gmail.com>
2022-10-27 15:04:01 +0000

    commit1

---
+++ src/components/GradientDirectionItem/index.js
@@ -0,0 +1,23 @@
+// Write your code here
+import {CustomButton, ListItem} from './styledComponents'
+
+const GradientDirectionItem = props => {
+  const {gradientDirectionDetails, isActive, clickGradientDirectionItem} = props
+  const {displayText, value} = gradientDirectionDetails
+  const onClickGradientDirectionItem = () => {
+    clickGradientDirectionItem(value)
+  }
+  return (
+    <ListItem>
+      <CustomButton
+        isActive={isActive}
+        type="button"
+        onClick={onClickGradientDirectionItem}
+      >
+        {displayText}
+      </CustomButton>
+    </ListItem>
+  )
+}
+
+export default GradientDirectionItem
